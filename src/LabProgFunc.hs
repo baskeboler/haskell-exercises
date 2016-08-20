958dc4ba3b489913b1e83120706ac63dee06b9df
Victor Gil <baskeboler@gmail.com>
2016-08-20 05:30:01 +0000

    lab

--- src/LabProgFunc.hs
+++ src/LabProgFunc.hs
@@ -8,7 +8,7 @@ module LabProgFunc where
 import Control.Applicative (pure)
 import Control.DeepSeq (NFData, ($!!))
 import Control.Monad (void)
-import Data.Map (Map, empty, foldMapWithKey, singleton)
+import Data.Map (Map, empty, foldWithKey, singleton)
 import GHC.Generics (Generic)
 import System.Environment (getArgs, getProgName)
 import System.IO (hPutStrLn, stderr)
@@ -101,3 +101,91 @@ altura
         e
             | binaria e ->
                 1 + max (altura $ operando1 e) (altura $ operando2 e)
+                
+-- suma e1 e2 = Suma e1 e2                
+
+newtype Fix f = Fx (f (Fix f)) 
+
+-- outF :: ExprF a
+-- outF (Fx (f (Fix f))) 
+    -- = f (Fix f)
+
+data ExprF a 
+    = SumaF a a
+    | RestaF a a 
+    | MultF a a
+    | DivF a a 
+    | NegF a
+    | Const Int 
+    deriving (Eq, Show, Read)
+
+instance Functor ExprF where
+    fmap f (Const e) = Const e 
+    fmap f (NegF e) = NegF (f e)
+    fmap f (SumaF a b) = SumaF (f a) (f b)
+    fmap f (RestaF a b) = RestaF (f a) (f b)
+    fmap f (MultF a b) = MultF (f a) (f b)
+    fmap f (DivF a b) = DivF (f a) (f b)
+    
+type Expr = Fix ExprF 
+    -- deriving (Eq, Read, Show)
+instance Show Expr where
+    show (Fx e) = "Fx " ++ show e 
+    
+type Algebra f a = f a -> a
+type SimpleA = Algebra ExprF Int
+alg :: SimpleA
+
+-- alg :: ExprF Int -> Int
+alg (Const i) = i
+alg (NegF n) = -n 
+alg (SumaF n m) = n + m
+alg (RestaF n m) = n - m
+alg (MultF n m) =  n *  m
+alg (DivF n m) = n `div` m
+
+type ExprInitAlg = Algebra ExprF (Fix ExprF)
+
+ex_init_alg :: ExprF (Fix ExprF) -> Fix ExprF
+ex_init_alg = Fx
+
+unFix :: Fix f -> f (Fix f)
+unFix (Fx x)=x
+
+g = alg . (fmap g) . unFix
+cata :: Functor f => Algebra f a -> Fix f -> a 
+cata alg = alg . fmap (cata alg) . unFix
+
+eval :: Fix ExprF -> Int
+eval = alg . (fmap eval) . unFix
+
+-- suma' :: Expr -> Expr -> Expr
+-- suma' n = cata phi where
+--     phi (Const m) = n+n
+--     phi (Cons m) = s m 
+    
+-- mult' :: Expr -> Expr -> Expr
+-- mult' n = cata phi where 
+--     phi (Literak 0) = z
+--     phi (Cons m) = suma' n m
+    
+-- s :: Expr -> Expr
+-- s = Fx . Fix
+z :: Expr 
+z = Fx (Const 0)
+cataExpresión
+    :: (a -> a -> a)
+    -> (a -> a -> a)
+    -> (a -> a -> a)
+    -> (a -> a -> a)
+    -> (a -> a)
+    -> (Integer -> a)
+    -> Expresión -> a
+cataExpresión
+    suma
+    resta
+    multiplicación
+    división
+    negativo
+    literal
+    = undefined
